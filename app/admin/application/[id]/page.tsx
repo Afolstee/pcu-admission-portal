ce4e5d5e19e3d6a4f40ba2841cc26b61483366ac
afolstee <afolstee@gmail.com>
2026-02-17 21:08:24 +0100

    fixed page.tsx

--- app/admin/application/[id]/page.tsx
+++ app/admin/application/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
+import Image from "next/image";
 import Link from "next/link";
 import { useRouter, useParams } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";

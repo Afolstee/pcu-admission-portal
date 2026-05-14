7411af4a2243d20500bbad6495ce6e0de0b3551a
Afolstee <afolstee@gmail.com>
2026-05-14 22:02:52 +0100

    updated

--- app/PartTime/PartTimeHero.tsx
+++ app/PartTime/PartTimeHero.tsx
@@ -3,7 +3,7 @@ export default function PartTimeHero() {
     <div className="relative w-full h-64 md:h-80 overflow-hidden">
       {/* Background image */}
       <img
-        src="e-portal/images/students.jpg"
+        src="/e-portal/images/students.jpg"
         alt="Part Time Programmes"
         className="absolute inset-0 w-full h-full object-cover"
       />
